67385138a1aa0002dfa107089f31f32ef1eb6d04
jasonchuang56 <jchuang@health2sync.com>
2020-05-28 18:51:18 +0800

    ahead master

--- xyz/ex.c
+++ xyz/ex.c
@@ -3,7 +3,18 @@
 
 #include <math.h>
 
+void func(void);
+
 int main(int argc, const char *argv[]) {
 	printf("This is a GIT Meger Example!!\n");
+	
+	func();
 	exit(0);
+}
+
+void func(void) {
+	for(int i=0; i<10; i++) {
+		printf("THE VALUE i = %d\n", i);
+
+	}
 }

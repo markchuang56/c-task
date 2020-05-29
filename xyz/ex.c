4456eee50412da4ea3f751bcbf06bb037943b9c4
jasonchuang56 <jchuang@health2sync.com>
2020-05-29 11:18:24 +0800

    complex v0.0.1

--- xyz/ex.c
+++ xyz/ex.c
@@ -4,10 +4,23 @@
 #include <math.h>
 
 void func(void);
+#define N		8
+struct Complex {
+	double real;
+	double imag;
+};
+
 
 int main(int argc, const char *argv[]) {
 	printf("This is a GIT Meger Example!!\n");
 	
+	struct Complex complex;
+	for(int i=0; i<N; i++) {
+		complex[i].real = cos(i/N);
+		complex[i].imag = sin(i/N);
+		printf("=========== Complex Test ===========\n");
+	}
+
 	func();
 	exit(0);
 }

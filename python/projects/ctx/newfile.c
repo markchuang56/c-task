9c275ec4d6ecdbc1d48efcb2bdba9343be7283ce
jasonchuang56 <jchuang@health2sync.com>
2020-07-27 11:49:36 +0800

    py-v 0.1

--- python/projects/ctx/newfile.c
+++ python/projects/ctx/newfile.c
@@ -2,7 +2,7 @@
 #include "stdlib.h"
 
 int main(int argc, const char *argv[]) {
-
+	printf("you'll learn what rebasing is, how to do it.\n");
 	printf("you can type something here ...");
 	exit(0);
 }

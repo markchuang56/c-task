f6f6bc265e0feef9a223f22ced68c08bd8a6d24c
jasonchuang56 <jchuang@health2sync.com>
2020-07-27 12:56:08 +0800

    ver 0.0

--- python/projects/ctx/cbin.c
+++ python/projects/ctx/cbin.c
@@ -1,7 +1,11 @@
 #include "stdio.h"
 #include "stdlib.h"
+#include "math.h"
 
 int main(int argc, const char *argv[]) {
 	printf("0123456789ABCDEF\n\n\n");
+	for(int i=1; i<argc; i++) {
+		printf("command %02d, %s\n", i, argv[i]);
+	}
 	exit(0);
 }
